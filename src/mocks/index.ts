64b9fcc0e48faa3c0634bfecf685476ddf01117e
Choleski Louis <louischoleski@gmail.com>
2024-07-05 10:41:05 -0400

    Update mock constants with search required fields.
    - Added default location and mock data for testing purposes.
    - Organized mock images and description for display in testing scenarios.
    - Updated search filters and weights to reflect required fields for distance, review score, host response rate, and extension flexibility.

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -4,25 +4,62 @@ import imgThree from '../../public/assets/img/rooms/pexels-photo-5178080.jpeg';
 import imgFour from '../../public/assets/img/rooms/pexels-photo-7195857.jpeg';
 import imgFive from '../../public/assets/img/rooms/pexels-photo-14715846.jpeg';
 
+// Constants for search and calculations.
 export const MAX_DISTANCE_KM:number = 100;
-
 export const EARTH_RADIUS_KM: number = 6371;
 
+// Search filters.
+export const MIN_DISTANCE: number = 0;
+export const MIN_REVIEW_SCORE: number = 0;
+export const MIN_HOST_RESPONSE_RATE: number = 0;
+export const MIN_EXTENSION_FLEXIBILITY: number = 0;
+
+export const MAX_DISTANCE: number = 100;
+export const MAX_REVIEW_SCORE: number = 5;
+export const MAX_HOST_RESPONSE_RATE: number = 1;
+export const MAX_EXTENSION_FLEXIBILITY: number = 1;
+
+// Search weights configuration.
+export const SEARCH_WEIGHTS = {
+	distance: {
+		isFloating: false,
+		min: MIN_DISTANCE,
+		max: MAX_DISTANCE,
+	},
+	review_score: {
+		isFloating: true,
+		min: MIN_REVIEW_SCORE,
+		max: MAX_REVIEW_SCORE,
+	},
+	host_response_rate: {
+		isFloating: true,
+		min: MIN_HOST_RESPONSE_RATE,
+		max: MAX_HOST_RESPONSE_RATE,
+	},
+	extension_flexibility: {
+		isFloating: true,
+		min: MIN_EXTENSION_FLEXIBILITY,
+		max: MAX_EXTENSION_FLEXIBILITY,
+	},
+};
+
+// Default search weights.
 export const DEFAULT_SEARCH_WEIGHTS: {
 	distance: number,
 	review_score: number,
 	host_response_rate: number,
 	extension_flexibility: number,
 } = {
-	distance: 0.3,
+	distance: 20,
 	review_score: 0.4,
 	host_response_rate: 0.2,
 	extension_flexibility: 0.1,
 };
-
-// Montréal position.
+// Default location (Montréal).
 export const MOCK_DEFAULT_LOCATION: { latitude: number, longitude: number } = { latitude: 45.508888, longitude: -73.561668 };
 
+// Mock images for testing/display.
 export const MOCK_IMAGES: string[] = [ imgOne, imgTwo, imgThree, imgFour, imgFive ];
 
+// Mock description for testing/display.
 export const MOCK_DESCRIPTION: string = 'Lorem ipsum dolor sit amet. Vel dolores dignissimos sit voluptas enim eum autem quas ea dicta natus! Est aperiam autem qui ullam nostrum ut ipsam esse et dignissimos voluptate. Qui soluta corrupti vel dolor expedita est quia possimus.';
