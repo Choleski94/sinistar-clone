4b03fdbab2e65c2216fabb7fd794bec610d015b8
Choleski Louis <louischoleski@gmail.com>
2024-07-06 09:00:32 -0400

    Refactor InfoCard Component to Support Grid View
    - Improved component reusability and readability.
    - Modified the InfoCard component to enable grid view functionality.
    - Ensured responsive design compatibility across different screen sizes.
    - Updated component structure and styles to align with grid layout requirements.

--- src/components/InfoCard/InfoCard.styled.tsx
+++ src/components/InfoCard/InfoCard.styled.tsx
@@ -1,45 +1,15 @@
 import { styled } from '@mui/system';
-import { Card, CardContent, CardMedia } from '@mui/material';
+import { CardMedia, Box } from '@mui/material';
 
-export const StyledCard = styled(Card)(({ theme }) => ({
-	display: 'flex',
-	padding: '16px',
-	boxShadow: 3,
-	marginBottom: '24px',
-	borderRadius: '16px',
-	flexDirection: 'column',
-	border: '1px solid #e5e7eb',
-	transition: 'transform 0.2s, box-shadow 0.2s',
-	'&:hover': {
-		boxShadow: 6,
-		transform: 'scale(1.01)',
-	},
-	[theme.breakpoints.up('md')]: {
-		flexDirection: 'row',
-	},
-}));
-
-export const StyledCardMedia = styled(CardMedia)(({ theme }) => ({
+export const StyledCardMedia = styled(CardMedia)({
 	width: '100%',
-	height: '15rem',
 	flexShrink: 0,
-	borderRadius: '16px',
+	height: '15rem',
 	marginBottom: '16px',
-	[theme.breakpoints.up('sm')]: {
-		height: '20rem',
-	},
-	[theme.breakpoints.up('md')]: {
-		height: '14rem',
-		width: '20rem',
-		marginBottom: 0,
-	},
-}));
+});
 
-export const StyledCardContent = styled(CardContent)(({ theme }) => ({
-	flexGrow: 1,
-	paddingLeft: 0,
-	[theme.breakpoints.up('md')]: {
-		paddingLeft: '24px',
-	},
-}));
-
+export const StyledInfoBox = styled(Box)({
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'space-between',
+});
--- src/components/InfoCard/InfoCard.tsx
+++ src/components/InfoCard/InfoCard.tsx
@@ -1,51 +1,40 @@
+import React from 'react';
 import { Star as StarIcon } from '@mui/icons-material';
-import { Typography, Stack, Box, Link } from '@mui/material';
+import { Card, CardContent, Typography, Stack, Box } from '@mui/material';
 
-import { 
-	StyledCard,
-	StyledCardMedia,
-	StyledCardContent,
-} from './InfoCard.styled';
 import { IListingItem } from '@api/types';
 
-const InfoCard = ({ id, name, address, review_score }: IListingItem) => (
-	<Link href={`/rooms?roomId=${id}`} target="_blank" rel="noreferrer" underline="none">
-		<StyledCard>
-			<StyledCardMedia
-				alt={name} component="img"
-				image="https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-			/>
-			<StyledCardContent>
-			<Box display="flex" justifyContent="space-between" alignItems="center">
-				<Typography variant="h6" color="textSecondary">
+import {
+	StyledInfoBox,
+	StyledCardMedia,
+} from './InfoCard.styled';
+
+// TODO: Use `id` props to create a link.
+const InfoCard = ({ name, address, review_score }: IListingItem) => (
+	<Card>
+		<StyledCardMedia
+			image="https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+			title={name}
+		/>
+		<CardContent>
+			<StyledInfoBox>
+				<Typography variant="h6" component="div">
 					{name}
 				</Typography>
-			</Box>
-			<Box display="flex" justifyContent="space-between" alignItems="center">
-				<Box>
-					<Typography variant="subtitle1">
-						{address}
-					</Typography>
-				</Box>
 				<Stack direction="row" alignItems="center" spacing={1}>
 					<StarIcon />
 					<Typography variant="body2">
 						{review_score}
 					</Typography>
 				</Stack>
-				</Box>
-				<Box mt={2} mb={2} width="10%">
-					<hr />
-				</Box>
-				<Typography variant="body2" color="textSecondary">
-					Lorem ipsum dolor sit amet. Vel dolores dignissimos sit voluptas enim eum autem quas ea dicta natus! 
-					Est aperiam autem qui ullam nostrum ut ipsam esse et dignissimos voluptate. 
-					Qui soluta corrupti vel dolor expedita est quia possimus.
+			</StyledInfoBox>
+			<Box display="flex" justifyContent="space-between" alignItems="center">
+				<Typography variant="h6" color="textSecondary">
+					{address}
 				</Typography>
-			</StyledCardContent>
-		</StyledCard>
-	</Link>
+			</Box>
+		</CardContent>
+	</Card>
 );
 
 export default InfoCard;
-
